d2fc03e5c5ef64662dd4b69e24cb8edd143aa4a4
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2018-05-01 11:42:39 -0300

    comment code

--- Codigo/TCC_code/TCC_code.ino
+++ Codigo/TCC_code/TCC_code.ino
@@ -19,23 +19,23 @@
 
 #define DHTTYPE DHT11
 
+// Comment to show in Serial Monitor 
 //#define BLUETOOTH_ON
 
-SoftwareSerial bSerial(11, 10);
-DHT dht(DHTPIN, DHTTYPE);
+SoftwareSerial bSerial(11, 10); // Emulate port (Rx, Tx) to use with hc-06
+
+DHT dht(DHTPIN, DHTTYPE); 
 MQ135 mq135_sensor = MQ135(PIN_MQ135);
 
 
-//const int NO2=1; //NO2 sensor on analog 3
 float Vout = 0; //output voltage
 float Rs = 0; //Sensor Resistance
 float ppbNO2 = 0; //ppb NO2
-float ppmNO2 =0;
+float ppmNO2 =0;  //ppm NO2
 
 
 void setup()
 {
-  // initialize serial communications and wait for port to open:
   Serial.begin(9600);
   bSerial.begin(9600);
   dht.begin();
@@ -44,43 +44,45 @@ void setup()
 
 void loop()
 {
+  // Get temp and humidity values with DHT11
   float temperature= dht.readTemperature();
   float humidity= dht.readHumidity();
 
+  // Get sensor CO2 Resistence (RS/R0) to convert values in PPM
   float rzero = mq135_sensor.getRZero();
   float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
   float resistance = mq135_sensor.getResistance();
   float ppm = mq135_sensor.getPPM();
   float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
       
-  // read NO2 sensor:
+  // Read NO2 sensor:
   Vout = analogRead(PIN_MICS2714)/409.2; // take reading and convert ADC value to voltage
   Rs = 22000/((5/Vout) - 1);   // find sensor resistance from Vout, using 5V input & 22kOhm load resistor
   ppbNO2 = (.000008*Rs - .0194);//*1000;    //convert Rs to ppb concentration NO2 (equation derived from data found on http://airpi.es/sensors.php
   ppmNO2 = ppmNO2/1000; //ppb to ppm
     
-  //if(Serial.available()){
-    
+     
 #ifdef BLUETOOTH_ON
-    bSerial.print(temperature);
-    bSerial.print(", ");
-    bSerial.print(humidity);
-    bSerial.print(", ");
-    bSerial.print(ppbNO2);
-    bSerial.print(", ");
-    bSerial.println(correctedPPM);
+
+  bSerial.print(temperature);
+  bSerial.print(", ");
+  bSerial.print(humidity);
+  bSerial.print(", ");
+  bSerial.print(ppbNO2);
+  bSerial.print(", ");
+  bSerial.println(correctedPPM);
+
 #else
 
-    Serial.print(temperature);
-    Serial.print(", ");
-    Serial.print(humidity);
-    Serial.print(", ");
-    Serial.print(ppbNO2);
-    Serial.print(", ");
-    Serial.println(ppm);
+  Serial.print(temperature);
+  Serial.print(", ");
+  Serial.print(humidity);
+  Serial.print(", ");
+  Serial.print(ppbNO2);
+  Serial.print(", ");
+  Serial.println(ppm);
+
 #endif  
-    delay(1000);
-  //}
+
+  delay(1000);
 }
-
-
